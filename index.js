9addafd8d6607c1ad55b9f0c710f07b1d536920a
Kematia <tommasobartolucci11@gmail.com>
2019-02-04 09:18:07 +0100

    dev

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello Index!'))
+app.get('/', (req, res) => {
+    res.send('Hello Index!')
+    console.log('index hit')
+})
 
-app.get('/hello', (req, res) => res.send('Hello World!'))
+app.get('/hello', (req, res) => {
+    res.send('Hello World!')
+    console.log('hello hit')
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
